fc60e851043dc107b146fc145b8adc963fd821d5
AnnAanIT <annaantit2310@gmail.com>
2026-01-11 16:16:00 +0700

    Fix: Remove multiplier display from country selector

--- pages/Login.tsx
+++ pages/Login.tsx
@@ -342,7 +342,7 @@ export default function Login() {
                         <option value="">Select your country...</option>
                         {countries.map(country => (
                           <option key={country.code} value={country.code}>
-                            {country.name} (×{country.multiplier})
+                            {country.name}
                           </option>
                         ))}
                       </select>
